223d8d5754ee6932c7adf214a53bb11b369fa222
pt11241 <html23232@gmail.com>
2023-10-17 17:11:09 +0300

    -

--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,8 +1,7 @@
 long long change(int a, int b, int c, int d){
-    long long zaplatil = c * 100 + d, price = a * 100 + b;
+    long long zaplatil = (long long)c * 100 + (long long)d, price = (long long)a * 100 + (long long)b;
     if (zaplatil < price){
         return -1;
     }
-    long long sdacha = (c - a) * 100 + (d - b);
-    return  sdacha;
+    return zaplatil - price;
 }
