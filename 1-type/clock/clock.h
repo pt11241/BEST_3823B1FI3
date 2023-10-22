8b3e57c70eb1018bd91a3641e1bb34d588394ee1
pt11241 <html23232@gmail.com>
2023-10-22 22:34:57 +0300

    ghghg

--- 1-type/clock/clock.h
+++ 1-type/clock/clock.h
@@ -1,4 +1,18 @@
 int get_time(long long angle) {
-    return 0;
+    int hours = 0, min = 0, sec = 0;
+    int hours_c = 0;
+    if (angle % 6 == 0){
+        int sec_up = (int)angle / 6;
+        hours_c = sec_up % (3600);
+        hours = (sec_up / 3600) * 10000; 
+        sec = hours_c % 60; 
+        min = (hours_c / 60) * 100;
+        return hours + min + sec;
+
+    }
+    else return 0;
+
+    
+
 }
 
--- 1-type/seconds/seconds.h
+++ 1-type/seconds/seconds.h
@@ -1,25 +1,25 @@
 int get_years(int seconds) {
-    return 0; // Not implementation
+    return seconds / 32140800 + 1970; 
 }
 
 int get_month(int seconds) {
-    return 0; // Not implementation
+    return (seconds % 32140800) / 2678400; 
 }
 
 int get_day(int seconds) {
-    return 0; // Not implementation
+    return ((seconds % 32140800) % 2678400) / 86400; 
 }
 
 int get_hours(int seconds) {
-    return 0; // Not implementation
+    return (((seconds % 32140800) % 2678400) % 86400) / 3600; 
 }
 
 int get_minutes(int seconds) {
-    return 0; // Not implementation
+    return ((((seconds % 32140800) % 2678400) % 86400) % 3600) / 60; 
 }
 
 int get_seconds(int seconds) {
-    return 0; // Not implementation
+    return seconds % 60; 
 }
 
 
@@ -30,5 +30,5 @@ int get_magical_value(int seconds) {
     int hours = get_hours(seconds);
     int minutes = get_minutes(seconds);
     int second = get_seconds(seconds);
-    return 0; // Not implementation
+    return ((year + month)/(1 + day + second))*((year + month)/(1 + day + second))+((minutes + (minutes/(1 + hours)))/(1 + hours));
 }
