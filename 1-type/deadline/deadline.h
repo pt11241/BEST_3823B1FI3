328090d3cf24a0a9d1d236840c396c84ac505c45
pt11241 <html23232@gmail.com>
2023-10-23 00:00:01 +0300

    fd

--- 1-type/deadline/deadline.h
+++ 1-type/deadline/deadline.h
@@ -1,3 +1,16 @@
 int get_score(short score, int start, int stop, int now) {
-    return 0; // Not implementation
+    float r = score / 2, result = 0;
+    int answer = 0, res = 0;
+    if(stop <= now){
+        return (score + 1) / 2; 
+    } 
+    if(start >= now){
+        return score;
+    }
+    else((start < now)){
+        result = r / (stop - start) * (now - start) + 0.15;
+        res = result;
+        answer = score - res;
+    }
+    return answer;
 }
