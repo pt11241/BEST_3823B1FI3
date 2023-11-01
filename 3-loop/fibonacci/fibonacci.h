662f49ec3373adaf871f56a0e5ae79d265bc98aa
pt11241 <html23232@gmail.com>
2023-11-01 11:57:51 +0300

    ffgh

--- 3-loop/fibonacci/fibonacci.h
+++ 3-loop/fibonacci/fibonacci.h
@@ -1,12 +1,24 @@
-long fibo(int n){
-    long S = 0;
-    
-}
+#include <malloc.h>
 
 long fib(int m){
-    long S = 0;
-    for (int i = 0; i<=m;i++){
-        S+=fibo(i);
+    int *ryad_fib;
+    int n = 40;
+    ryad_fib = (int*)malloc(n*sizeof(int));
+    *ryad_fib = 0;
+    *(ryad_fib + 1) = 1;
+    for (int i = 2; i < n; i++){
+        *(ryad_fib + i) = *(ryad_fib + i - 1) + *(ryad_fib + i - 2);
     }
-    return S;
+    if (m < 0){
+        return -1;
+    }
+    long summa = 0;
+    for (int i = 0; i < n; i++){
+        if (*(ryad_fib + i) <= m){
+            summa+=*(ryad_fib + i);
+        }
+
+    }
+    free(ryad_fib);
+    return summa;
 }
