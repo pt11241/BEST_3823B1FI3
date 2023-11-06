08c084e56c3f261e1e1bd3160d660bfbd4094c2f
pt11241 <html23232@gmail.com>
2023-11-06 15:19:42 +0300

    c

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -8,21 +8,24 @@ unsigned long long calc_e(char order){
     int* arr = (int*)malloc(sizeof(int)*10);
     int i = 9;
     while (e_osn > 0){
-        *(arr + i) = e_osn % 10; // заполняешь массив
+        *(arr + i) = e_osn % 10; // заполням массив
         e_osn/=10;
         i--;
     }
+
     for (int i = 0; i < 10; i++){
         nado += arr[i] * pow(10,order);
         order--;
         if (order_c == i) break;
     }
+
     free(arr);
     unsigned long long order_2 = pow(10,(unsigned long long)order_c); // 10 в нужной степени
-    for (unsigned long long i = 1; i < 1000000; i++){
-        unsigned long long e = pow(1 + 1/i,i);
+    for (double i = 1; i < 1000000; i++){
+        double e = pow(1 + 1/i,i);
         if ((int)(e * order_2) == nado){
-            return i;
+            unsigned long long i_f= (unsigned long long)i;
+            return i_f;
         }
     }
     return 0;
