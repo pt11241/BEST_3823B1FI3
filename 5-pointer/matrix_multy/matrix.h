9da2aacb3585ab14cb7bacee876d86657d6eee6d
pt11241 <html23232@gmail.com>
2023-12-09 00:32:14 +0300

    wi

--- 5-pointer/matrix_multy/matrix.h
+++ 5-pointer/matrix_multy/matrix.h
@@ -4,15 +4,20 @@
 
 
 int* matrix_multy(int* A, int aM, int aN, int* B, int bM, int bN){
-    if (aM != bN) return NULL;
+    if (aN != bM) return NULL;
     int* new_array = (int*)malloc(sizeof(int) * aM * bN);
+    for (int i = 0; i < aM * bN; i++){
+        new_array[i] = 0;
+    }
+
      for (int i = 0; i < aM; i++){
         for (int j = 0; j < bN; j++){
             for (int k = 0; k < aN; k++){
-                new_array[i * aM + j] += A[i * aN + k] * B[k * bN + j];
-            }
+                new_array[i * aM + j] += A[i * aN + k]* B[k * bN + j];
+            }   
         }
     }
+    
     return new_array;
 }
  
