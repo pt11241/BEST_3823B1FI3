f3c2834881d00d40792c833ef428a15d6bc994f8
pt11241 <html23232@gmail.com>
2023-12-11 18:58:19 +0300

    tk

--- 6-functions/array_transform/transform_array.h
+++ 6-functions/array_transform/transform_array.h
@@ -1,5 +1,45 @@
 #include <math.h>
 
 void transform(double* array, int size, int* comands, int comands_count){
+    for (int i = 0; i < comands_count; i++){
+        if (comands[i] == 1){
+            for (int i = 0; i < size; i++) array[i] += 1;
+        }
+        if (comands[i] == 2){
+            for (int i = 0; i < size; i++) array[i] = pow(abs(array[i]),0.5);
+        }
+        if (comands[i] == 3){
+            double sr_ar = 0;  
+            for (int i = 0; i < size; i++){
+                sr_ar += array[i]/(double)size; // c size pr
+            }
+            for (int i = 0; i < size; i++){
+                array[i] = pow(abs(array[i]),sr_ar);
+            }
+        }
+        if (comands[i] == 4){
+            for (int i = 0; i < size; i++){
+                if ((int)array[i] % 2 == 0){
+
+                 array[i] = (int)array[i]*(-1);
+                }
+                if ((int)array[i] % 2 != 0) array[i] = 0;
+            }
+        }
+        if (comands[i] = 5){
+            for (int i = 0; i < size; i++){
+                if (((int)array[i]) % 2 == 0 && i % 2 == 0) array[i] = 0;
+            }
+        }
+        if (comands[i] == 6){
+            for (int i = 0; i < size / 2; i++){
+                if (array[i] < 0) array[i] = array[i]*(-1);
+                if (array[i] == 0) array[i] +=1;
+            }
+            for (int i = size / 2; i < size; i++){
+                array[i] = -1;
+            }
+        }
+    }
     return;
 }
--- 6-functions/kramer/kramer_method.h
+++ 6-functions/kramer/kramer_method.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
+int det23(int* arr, int size){
+    if (pow(size, 0.5) == 2) return arr[0]*arr[3] - arr[1]*arr[2];
+    if (pow(size, 0.5) == 3) return arr[0]*arr[4]*arr[8] + arr[1]*arr[5]*arr[6] + arr[2] * arr[3] * arr[7] - arr[2]*arr[4]*arr[6] 
+           - arr[1]*arr[3]*arr[8] - arr[0]*arr[5]*arr[7];
+}
+
+
 double kramer_method(int* matrix, int* vector, int size){
+    int det = det23(matrix,size);
+    if (det = 0) return 0;
+    for 
     return 100;
 }
