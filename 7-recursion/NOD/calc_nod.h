5958eda1f826dbdb5d119492e96b7d76570f5188
pt11241 <html23232@gmail.com>
2023-12-28 00:45:49 +0300

    ok

--- 7-recursion/NOD/calc_nod.h
+++ 7-recursion/NOD/calc_nod.h
@@ -1,5 +1,18 @@
 #include <math.h>
 
+int nod_pluce(int first, int sec, int num){
+    if (first == 0 && num != 0) return sec;
+    if (sec == 0 && num != 0) return first;
+    if((first == 0 || sec == 0) && num == 0) return -1;
+    if(sec >= first){
+        return nod_pluce(first, sec - (sec / first)*first, num + 1);
+    }
+    else{
+        return nod_pluce(first - (first / sec) * sec, sec,num + 1);
+    }
+}
 int nod(int first, int second){
-    return -1;
+    return nod_pluce(first,second,0);
+   
+    
 }
--- 7-recursion/revers_array/revers.h
+++ 7-recursion/revers_array/revers.h
@@ -1,5 +1,14 @@
 #include <math.h>
 
-void reverse_array(int* array, int array_size){
+void r_a(int* arr, int finish, int start){
+    int copy = arr[finish];
+    if (finish <= start) return;
+    arr[finish] = arr[start];
+    arr[start] = copy;
+    return r_a(arr,finish-1, start+1);
+}
 
+void reverse_array(int* array, int array_size){
+    if (array_size == 0) return;
+    r_a(array,array_size-1,0);
 }
