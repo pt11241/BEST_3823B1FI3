579146ff43efd16ec8282e327d383b4027718f45
pt11241 <html23232@gmail.com>
2023-12-29 02:37:55 +0300

    123

--- 1-type/decomposition/decomposition.h
+++ 1-type/decomposition/decomposition.h
@@ -1,6 +1,6 @@
 #include<math.h>
 int reverse(int number) {
-    int sum = 0, i = 1, num = number, io = -1;
+    int sum = 0, i = 0, num = number, io = 0;
     while (number > 0){
         i++;
         number /=10;
@@ -26,7 +26,7 @@ double decomposition(int number) {
     }   
     double arr[cnt];
     for (int i = 0; i < cnt; i++){
-        arr[i] = num % 10;
+        arr[i] = (num % 10) * pow(10,i);
         num /= 10;
     }
     double znam = 0;
@@ -38,5 +38,18 @@ double decomposition(int number) {
 }
 
 int append(int number, int start, int end) {
-    return 0; // Not implementation
+    int num = number;
+    int cnt_num = 0;
+    int res = 0;
+    while (number > 0){
+        cnt_num ++;
+        number /= 10;
+    }
+    if(start >= 0){
+        res = (start * pow(10,cnt_num) + num)*10 + end;
+    }
+    else{
+        res = (-1)*((abs(start) * pow(10,cnt_num) + num)*10 + end);
+    }
+    return res; 
 }
